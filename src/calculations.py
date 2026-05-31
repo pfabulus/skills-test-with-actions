8a253682474d658808f0bdcd368217c1baf01785
pfabulus <131535678+pfabulus@users.noreply.github.com>
2026-05-31 09:14:28 -0500

    Update calculations.py

--- src/calculations.py
+++ src/calculations.py
@@ -26,13 +26,4 @@ def get_nth_fibonacci(n):
             a, b = b, a + b
         return b
         
-def test_get_nth_fibonacci_ten():
- """Test with n=10."""
- # Arrange
- n = 10
 
- # Act
- result = get_nth_fibonacci(n)
-
- # Assert
- assert result == 89
